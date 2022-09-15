f881870c748eb2b76e234517155824e364068ded
PRIYANSHU PANDEY <86107841+Priyanshu88@users.noreply.github.com>
2022-09-16 01:44:40 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,13 @@ import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBQ-tcH_KSW1QtOutiWQ1Ra6zOLkX14RnU",
-    authDomain: "linkedin-clone-ecb64.firebaseapp.com",
-    projectId: "linkedin-clone-ecb64",
-    storageBucket: "linkedin-clone-ecb64.appspot.com",
-    messagingSenderId: "897468744128",
-    appId: "1:897468744128:web:353893430e401744c88825",
-    measurementId: "G-4DX7NK16SS"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 
